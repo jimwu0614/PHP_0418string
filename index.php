67c969c0ad417dd83d3e9b4a65d04515856ab018
chiachun wu <a0283137@gmail.com>
2022-04-18 14:27:02 +0800

    PHP字串

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP字串函式介紹</title>
+</head>
+<body>
+    <H1>常用字串函式</H1>
+    <h3>substr() / mb_substr()</h3>
+    <ul>
+        <li>substr() - 用在英文系統單字元字集</li>
+        <li>mb_substr() - 用在多文系統雙字元字集(multiple byte)</li>
+    </ul>
+    <h4></h4>
+</body>
+</html>
